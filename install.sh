54d429e0e1cb3545e4f7904a3de967e09118bff4
hinte019 <hinte019@umn.edu>
2023-07-12 10:50:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # use set x for verbose debug output. uncomment at top and bottom
 #set -x
-echo "----------Installing DDEV----------"
-curl -fsSL https://apt.fury.io/drud/gpg.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ddev.gpg > /dev/null
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/ddev.gpg] https://apt.fury.io/drud/ * *" | sudo tee /etc/apt/sources.list.d/ddev.list
-sudo apt update && sudo apt install -y ddev
 echo "----------What is the sitename? (folder name)----------"
 read sitename
 if [ -d "$sitename" ]
