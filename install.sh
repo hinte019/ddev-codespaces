f3c37d4c3ab3640439eb286fe4f4ed54aba430f0
hinte019 <hinte019@umn.edu>
2023-07-20 16:01:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # use set x for verbose debug output. uncomment at top and bottom
 set -x
-echo "----------We will need to add an SSH key to codespaces unti I can figure out a better method----------"
+echo "----------We will need to add an SSH key to codespaces until I can figure out a better method----------"
 echo "----------What is your full UMN Email? Ex. urweb@umn.edu----------"
 read email
 # Make ssh dir in home dir
@@ -14,10 +14,10 @@ echo "----------Start the SSH agent in the background--------------"
 eval "$(ssh-agent -s)"
 # Add your SSH private key to the SSH agent
 ssh-add /home/drupal/.ssh/id_ed25519
-# Copy the contents of the id_ed25519.pub file to your clipboard
-xclip -sel clip < /home/drupal/.ssh/id_ed25519.pub
-echo "----------Key has been copied to clipboard!--------------"
-echo "----------Please go to https://github.umn.edu/settings/ssh/new (CMD click on mac) and paste the key--------------"
+echo "----------Please go to https://github.umn.edu/settings/ssh/new (CMD click on mac) and paste the key and give a title--------------"
+cat ~/.ssh/id_rsa.pub
+echo "----------Press Enter when done----------"
+read wait1
 echo "----------What is the sitename? (folder name)----------"
 read sitename
 if [ -d "$sitename" ]
@@ -26,11 +26,9 @@ if [ -d "$sitename" ]
 	else
 		git clone -b 9.x-prod git@github.umn.edu:drupalplatform/d8-composer.git $sitename
 		cd $sitename
-		ddev config --project-type=php
-		ddev start
-		ddev composer install
+		composer install
 		cd docroot/sites/
-		echo "----------Please paste in the default git repo code copied from clipboard----------"
+		echo "----------Please paste in the Default Folder git repo code (Code->SSH->Copy) copied from clipboard and hit enter----------"
 		read gitrepo
 		if [ -d "default" ]
 			then
@@ -46,9 +44,11 @@ if [ -d "$sitename" ]
 		fi
 		cd ..
 		cd ..
-		mkcert -install
-		ddev config --project-type=drupal9
-		ddev restart
+		# mkcert -install
+  		# Gen pw
+    		PW=date +%s | sha256sum | base64 | head -c 32
+  		# Create db
+    		mysql -u admin -p $PW -e "CREATE DATABASE drupal;"
 		cd ..
 fi
 echo "----------Do you have a DB? (y/n)----------"
@@ -56,17 +56,40 @@ read dba
 if [ $dba == 'Yes' ] || [ $dba == 'yes' ] || [ $dba == 'Y' ] || [ $dba == 'y' ]
 	then
 		cd $sitename
-		echo "----------Please drag and drop the db here, then hit enter----------"
-		read dbpath
-		ddev import-db --src=$dbpath
+		echo "----------Please drag and drop the db into the new site folder and wait, then hit enter----------"
+		read wait2
+		# check for the presence of .sql file
+		sqlfile=$(find . -maxdepth 1 -name "*.sql" -print -quit)
+		
+		# check for the presence of .sql.tar.gz file
+		sqltarfile=$(find . -maxdepth 1 -name "*.sql.tar.gz" -print -quit)
+		
+		if [[ -n $sqlfile ]]; then
+		  # .sql file found, import it
+		  echo "Found .sql file, starting import..."
+		  drush sqlc --database='mysql://admin:$PW@localhost/drupal' < $sqlfile
+
+		elif [[ -n $sqltarfile ]]; then
+		  # .sql.tar.gz file found, unzip it and import
+		  echo "Found .sql.tar.gz file, starting extraction and import..."
+		  gunzip -c $sqltarfile > database-extracted.sql
+		  drush sqlc --database='mysql://admin:$PW@localhost/drupal' < database-extracted.sql
+		else
+		  # neither .sql nor .sql.tar.gz file found, print error message
+		  echo "Error: No .sql or .sql.tar.gz file found in the directory."
+    		  exit 1
+		fi
 		echo "----------Clearing cache----------"
-		ddev exec drush cr
+		drush cr
 		echo "----------Uninstalling prod modules----------"
-		ddev exec drush pm-uninstall -y simplesamlphp_auth memcache acquia_purge purge
+		drush pm-uninstall -y simplesamlphp_auth memcache acquia_purge purge
 		cd ..
 elif  [ $dba == 'No' ] || [ $dba == 'no' ] || [ $dba == 'N' ] || [ $dba == 'n' ]
 	then
-		echo "----------Please go to site URL above and Install. Hit enter when ready----------"
+ 		echo "----------Please enter a password for the site. Username is admin----------"
+   		read loginpw
+ 		drush si lightning_umn --db-url=mysql://admin:$PW@localhost/drupal --site-name='My Drupal Site' --account-name=admin --account-pass=$loginpw
+		echo "----------Site install finished!----------"
 		read asdf
 else
 	echo "----------Error with input!----------"
@@ -76,7 +99,7 @@ echo "----------Configure stage proxy? (y/n)----------"
 read stageproxy
 if [ $stageproxy == 'Yes' ] || [ $stageproxy == 'yes' ] || [ $stageproxy == 'Y' ] || [ $stageproxy == 'y' ]
 	then
-	ddev exec drush pm-enable -y stage_file_proxy
+	drush pm-enable -y stage_file_proxy
 	echo "----------Whats the Site name? (eg gradschool-d8 for gradschool-d8.dev.umn.edu)----------"
 	read stageurlname
 	echo "----------Dev, stg or Prd?----------"
@@ -86,26 +109,25 @@ if [ $stageproxy == 'Yes' ] || [ $stageproxy == 'yes' ] || [ $stageproxy == 'Y' 
 	read devorprd
 	if [ $devorprd == 1 ]
 		then
-		ddev exec drush cset -y stage_file_proxy.settings origin https://$stageurlname.dev.umn.edu
+		drush cset -y stage_file_proxy.settings origin https://$stageurlname.dev.umn.edu
 	elif [ $devorprd == 2 ]
 		then
-		ddev exec drush cset -y stage_file_proxy.settings origin https://$stageurlname.stg.umn.edu
+		drush cset -y stage_file_proxy.settings origin https://$stageurlname.stg.umn.edu
 	elif [ $devorprd == 3 ]
 		then
-		ddev exec drush cset -y stage_file_proxy.settings origin https://$stageurlname.umn.edu
+		drush cset -y stage_file_proxy.settings origin https://$stageurlname.umn.edu
 	else
 		echo "----------wrong input detected----------"
 	fi
-	ddev exec drush cset -y stage_file_proxy.settings verify 0
-	ddev exec drush cset -y stage_file_proxy.settings origin_dir sites/$stageurlname.umn.edu/files
+	drush cset -y stage_file_proxy.settings verify 0
+	drush cset -y stage_file_proxy.settings origin_dir sites/$stageurlname.umn.edu/files
 fi
 echo "----------Configuring cache settings----------"
-ddev exec drush cset -y system.file path.temporary /tmp
-ddev exec drush -y config-set system.performance css.preprocess 0
-ddev exec drush -y config-set system.performance js.preprocess 0
+drush cset -y system.file path.temporary /tmp
+drush -y config-set system.performance css.preprocess 0
+drush -y config-set system.performance js.preprocess 0
 echo "Overwriting your-site/docroot/sites/development.services.yml"
 cat <<EOF > docroot/sites/development.services.yml
-
 parameters:
   http.response.debug_cacheability_headers: true
   twig.config:
@@ -129,11 +151,10 @@ echo "----------Run db updates? (y/n)----------"
 read dba
 if [ $dba == 'Yes' ] || [ $dba == 'yes' ] || [ $dba == 'Y' ] || [ $dba == 'y' ]
 	then
-		ddev exec drush updb
-		ddev exec drush cr
+		drush updb
+		drush cr
 fi
-ddev restart
 echo "----------Generating login link----------"
-ddev exec drush uli
+drush uli
 echo "----------Finished!----------"
 set +x
